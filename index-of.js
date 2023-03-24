d83c33a35077e9bfdd6c1f6baddb256713bdd854
Mathéo Lang <matheo.lang@ynov.com>
2023-03-24 16:44:06 +0100

    indexof

--- index-of.js
+++ index-of.js
@@ -9,7 +9,7 @@ function indexOf(arr,val,opt=0){
 
 function lastIndexOf(arr,val,opt=arr.length-1){
     let rep=-1;
-    for(let i=opt;i>=0;i--){
+    for(let i=opt;i>0;i--){
         if(arr[i]===val){
             rep=i;
         }
