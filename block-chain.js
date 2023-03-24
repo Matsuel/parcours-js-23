8421e6e7adde1f68533ba33dbd7cfc92ddbd93ac
Mathéo Lang <matheo.lang@ynov.com>
2023-03-24 15:12:32 +0100

    blockchain

--- block-chain.js
+++ block-chain.js
@@ -6,7 +6,7 @@ const hashCode = str =>
 function blockChain(data, prev={index:0, hash:'0'}){
     const index = prev.index + 1;
     const hash = hashCode(index + prev.hash+ JSON.stringify(data));
-    const chain = (prev) => blockChain(prev);
+    const chain = ({index, hash},data) => blockChain(prev={index, hash}, data);
     return {index, hash, data, prev, chain};
 }
 
