ae089f6234c6e5044a9f27556abe0fdd6f0cc808
Mathéo Lang <matheo.lang@ynov.com>
2023-03-24 14:38:25 +0100

    blockchain

--- block-chain.js
+++ block-chain.js
@@ -3,16 +3,29 @@ const hashCode = str =>
     [...str].reduce((h, c) => (h = (h << 5) - h + c.charCodeAt(0)) & h, 0) >>> 0
   ).toString(36)
 
-function blockChain(data, prev={index:0, hash:'0'}){
-    const index = prev.index + 1;
-    const hash = hashCode(index + prev.hash+ JSON.stringify(data));
+function blockChain(data, previ={index:0, hash:'0'}){
+    const index = previ.index + 1;
+    const prev = previ;
+    const hash = hashCode(index + previ.hash+ JSON.stringify(data));
     const chain = data => blockChain(data, {index, hash});
     return {index, data, prev, hash, chain};
 }
 
-const first = blockChain({ a: 1 })
-console.log(first.index) //           -> 1
-console.log(first.data) //            -> { a: 1 }
-console.log(first.prev) //            -> { index: 0, hash: "0" }
-console.log(first.hash) //            -> '1103f27'
-console.log(hashCode('10{"a":1}')) // -> '1103f27'
+// const first = blockChain({ a: 1 })
+// console.log(first.index) //           -> 1
+// console.log(first.data) //            -> { a: 1 }
+// console.log(first.prev) //            -> { index: 0, hash: "0" }
+// console.log(first.hash) //            -> '1103f27'
+// console.log(hashCode('10{"a":1}')) // -> '1103f27'
+
+const block = blockChain({ a: 1 }).chain({ hello: 'world' })
+
+const chain = block
+  .chain({ value: 4455 })
+  .chain({ some: 'data' })
+  .chain({ cool: 'stuff' })
+
+const fork = block
+  .chain({ value: 335 })
+  .chain({ some: 'data' })
+  .chain({ cool: 'stuff' })
