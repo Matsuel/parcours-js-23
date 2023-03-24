72ec70652087d1125aef092cd6a69a055c9ae79f
Mathéo Lang <matheo.lang@ynov.com>
2023-03-24 15:59:53 +0100

    elementary

---
+++ elementary.js
@@ -0,0 +1,40 @@
+function multiply(a,b){
+    let rep=0;
+    while (a>0){
+        a--;
+        rep+=b;
+    }
+    return rep;
+}
+
+function divide(a,b){
+    let rep=0;
+    if (a>b){
+        while (a>=0){
+            a-=b;
+            rep++;
+        }
+    }else{
+        while(b>=0){
+            b-=a;
+            rep++;
+        }
+    }
+    return rep;
+}
+
+function modulo(a,b){
+    let rep=0;
+    if (a>b){
+        while (a>=0){
+            a-=b;
+            rep++;
+        }
+    }else{
+        while(b>=0){
+            b-=a;
+            rep++;
+        }
+    }
+    return rep;
+}
