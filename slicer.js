d5697d65ebf5155de30e5e70782b71aa8abf6e2a
Mathéo Lang <matheo.lang@ynov.com>
2023-03-24 17:10:33 +0100

    slice

--- slicer.js
+++ slicer.js
@@ -1,4 +1,4 @@
-function slice(element, start=0, end=element.length-1){
+function slice(element, start=0, end=element.length){
     let rep=[];
     if (start<0){
         start=element.length+start;
@@ -7,12 +7,12 @@ function slice(element, start=0, end=element.length-1){
         end=element.length+end;
     }
     if (element instanceof Array){
-        for (let i=start;i<=end;i++){
+        for (let i=start;i<=end-1;i++){
             rep.push(element[i]);
         }
         return rep;
     }else{
-        for (let i=start;i<=end-1;i++){
+        for (let i=start;i<=end;i++){
             rep.push(element.charAt(i));
         }
         return rep.join("");
@@ -22,4 +22,4 @@ function slice(element, start=0, end=element.length-1){
 
 console.log(slice([1, 2, 3, 4, 5, 4, 3, 2, 1], 2, 4));
 console.log(slice("12345", 2));
-console.log(slice('abcdef', 0, 2))
+console.log(slice('abcdef', 2))
