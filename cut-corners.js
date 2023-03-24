f5cd12899c2c6c416dab3bba696a4647ed1f9c10
Mathéo Lang <matheo.lang@ynov.com>
2023-03-24 17:21:20 +0100

    cut-corners

---
+++ cut-corners.js
@@ -0,0 +1,63 @@
+function round(x){
+    let rep=0;
+    if (x<0){
+        while (x<=0){
+            x+=1;
+            rep+=1;
+        }
+    }else{
+        while (x>=1){
+            x-=1;
+            rep+=1;
+        }
+    }
+    return rep;
+}
+
+function ceil(x){
+    let rep=0;
+    if (x<0){
+        while (x<0){
+            x+=1;
+            rep+=1;
+        }
+    }else{
+        while (x>1){
+            x-=1;
+            rep+=1;
+        }
+    }
+    return rep;
+}
+
+function floor(x){
+    let rep=0;
+    if (x<0){
+        while (x<=-1){
+            x+=1;
+            rep+=1;
+        }
+    }else{
+        while (x>=0){
+            x-=1;
+            rep+=1;
+        }
+    }
+    return rep;
+}
+
+function trunc(x){
+    let rep=0;
+    if (x<0){
+        while (x<0){
+            x+=1;
+            rep+=1;
+        }
+    }else{
+        while (x>0){
+            x-=1;
+            rep+=1;
+        }
+    }
+    return rep;
+}
