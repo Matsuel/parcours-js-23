54cb67d3630118d1c94b2d763bf3f75d07340fe7
Mathéo Lang <matheo.lang@ynov.com>
2023-03-24 13:31:15 +0100

    physics

---
+++ physics.js
@@ -0,0 +1,11 @@
+function getAcceleration(obj){
+    if (obj.f!=0 && obj.m!=0){
+        return obj.f/obj.m;
+    }else if (obj.Δv!=0 && obj.Δt!=0){
+        return obj.Δv/obj.Δt;
+    }else if (obj.t!=0 && obj.d!=0){
+        return 2*obj.d/obj.t**2;
+    }else{
+        return "impossible"
+    }
+}
