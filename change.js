5cb4286afb93c2fea0fba5819419c917b762874b
Mathéo Lang <matheo.lang@ynov.com>
2023-03-24 10:49:53 +0100

    change

--- change.js
+++ change.js
@@ -6,10 +6,3 @@ function set(key, value){
     sourceObject[key] = value;
     return get(key);
 }
-
-const sourceObject = {
-    num: 42,
-    bool: true,
-    str: 'some text',
-    log: console.log,
-}
