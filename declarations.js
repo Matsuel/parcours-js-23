fcc2f38c086fed8845ce042ca367d2700ef03d9b
Mathéo Lang <matheo.lang@ynov.com>
2023-03-24 10:16:36 +0100

    declarations

---
+++ declarations.js
@@ -0,0 +1,23 @@
+const escapeStr= String.fromCharCode(96,92,47,34,39)
+
+const arr= [4,'2']
+
+const obj= {
+    str : "",
+    num : 0,
+    bool : false,
+    undef : undefined,
+}
+
+const nested={
+    arr:[4,undefined,'2'],
+    obj:{
+        str : "",
+        num : 0,
+        bool : false,
+    }
+}
+
+Object.freeze(obj);
+Object.freeze(nested);
+Object.freeze(arr);
