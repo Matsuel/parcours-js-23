cdbc25cf0ce4e5a514ecdaf4f275ce7738df1a42
Mathéo Lang <matheo.lang@ynov.com>
2023-03-24 11:04:00 +0100

    dogYears

--- dog-years.js
+++ dog-years.js
@@ -1,22 +1,21 @@
 function dogYears(planet,age){
-    age =age/31557600;
     let rep=0;
     if (planet==="earth"){
-        rep=age*7;
+        rep=(age/31557600)*7;
     }else if (planet==="mercury"){
-        rep=age*7*0.2408467;
+        rep=age*7/0.2408467/31557600;
     }else if (planet==="venus"){
-        rep=age*7*0.61519726;
+        rep=age*7/0.61519726/31557600;
     }else if (planet==="mars"){
-        rep=age*7*1.8808158;
+        rep=age*7/1.8808158/31557600;
     }else if (planet==="jupiter"){
-        rep=age*7*11.862615;
+        rep=age*7/11.862615/31557600;
     }else if (planet==="saturn"){
-        rep=age*7*29.447498;
+        rep=age*7/29.447498/31557600;
     }else if (planet==="uranus"){
-        rep=age*7*84.016846;
+        rep=age*7/84.016846/31557600;
     }else if (planet==="neptune"){
-        rep=age*7*164.79132;
+        rep=age*7/164.79132/31557600;
     }
 
     rep= parseFloat(rep).toFixed(2);
@@ -24,3 +23,4 @@ function dogYears(planet,age){
 }
 
 console.log(dogYears("earth", 1000000000));
+console.log(dogYears("mercury", 2134835688));
