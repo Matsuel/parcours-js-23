0a27878e73dfa33dba3d977d5b39e863308253e3
Mathéo Lang <matheo.lang@ynov.com>
2023-03-24 15:47:15 +0100

    blockchain

--- block-chain.js
+++ block-chain.js
@@ -4,26 +4,31 @@ const hashCode = str =>
   ).toString(36)
 
 function blockChain(data, prev={index:0, hash:'0'}){
-    const index = prev.index + 1;
-    const hash = hashCode(index + prev.hash+ JSON.stringify(data));
+    let block = {
+        data,
+        prev
+    }
+    block.index = block.prev.index + 1;
+    block.hash = hashCode(block.index + prev.hash+ JSON.stringify(data));
     // const chain = () => blockChain({index, hash}, {index, hash, data, prev, chain});
-    const chain = (data) => blockChain(data, {index, hash, data, prev, chain});
-    return {index, hash, data, prev, chain};
+    block.chain = (data) => blockChain(data, block);
+    // console.log({index, hash, data, prev, chain})
+    return block;
 }
 
 const block = blockChain({ a: 1 }).chain({ hello: 'world' })
 
-console.log(block);
+// console.log(block);
 
-// const chain = block
-//   .chain({ value: 4455 })
-//   .chain({ some: 'data' })
-//   .chain({ cool: 'stuff' })
+const chain = block
+  .chain({ value: 4455 })
+  .chain({ some: 'data' })
+  .chain({ cool: 'stuff' })
 
-// const fork = block
-//   .chain({ value: 335 })
-//   .chain({ some: 'data' })
-//   .chain({ cool: 'stuff' })
+const fork = block
+  .chain({ value: 335 })
+  .chain({ some: 'data' })
+  .chain({ cool: 'stuff' })
 
 
 // console.log(chain)
