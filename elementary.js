101fac96e929abd53b5633c1fa15f92df40602d4
Mathéo Lang <matheo.lang@ynov.com>
2023-03-24 16:32:10 +0100

    elementary

--- elementary.js
+++ elementary.js
@@ -1,45 +1,27 @@
-function multiply(a,b){
+function multiply(a, b) {
     let rep=0;
-    if (a<0){
-        while (a<0){
-            a++;
-            rep+=b;
-        }
-        return -rep;
-    }else{
-        while (a>0){
-            a--;
-            rep+=b;
-        }
-        return rep;
+    for (let i=0; i<b; i++) {
+        rep += Math.abs(a);
     }
+    return (a<0 && b<0 || a>0 && b>0) ? rep : -rep;
 }
 
-function divide(a,b){
+function divide(a, b) {
     let rep=0;
-    if (a>b){
-        while (a>=b){
-            a-=b;
-            rep++;
-        }
-        return rep;
-    }else{
-        return 0;
+    let absa = Math.abs(a);
+    let absb = Math.abs(b);
+    while (absa >= absb) {
+        absa -= absb;
+        rep++;
     }
-    
+    return (a<0 && b<0 || a>0 && b>0) ? rep : -rep;
 }
 
-function modulo(a,b){
-    if (a>b){
-        while (a>0){
-            a-=b;
-        }
-        return a;
-    }else{
-        return a;
+function modulo(a, b) {
+    let absa = Math.abs(a);
+    let absb = Math.abs(b);
+    while (absa >= absb) {
+        absa -= absb;
     }
+    return (a<0) ? -absa : absa;
 }
-
-console.log(divide(78,34))
-console.log(modulo(78,34))
-console.log(modulo(34,78))
