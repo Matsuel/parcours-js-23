ace3a72a0ed56d9a65c4f48fe22a3abd82683820
Mathéo Lang <matheo.lang@ynov.com>
2023-03-26 20:55:31 +0200

    cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -44,46 +44,51 @@ function ceil(x){
     }
 }
 
-function floor(x){
+// function floor(x){
+//     let isneg = false;
+//     if (x<0){
+//         isneg = true;
+//         x = -x;
+//     }
+//     let nb =0;
+//     while (!(x < 1 && x > -1)){
+//         x--;
+//         nb++;
+//     }
+//     if (isneg){
+//         return -nb-1;
+//     }else{
+//         return nb;
+//     }
+// }
+
+function trunc(x){
+    let nb =0;
+    if (x> 0xffffffff){
+        x -= 0xffffffff;
+        nb += 0xffffffff;
+    }
     let isneg = false;
     if (x<0){
         isneg = true;
         x = -x;
     }
-    let nb =0;
     while (!(x < 1 && x > -1)){
         x--;
         nb++;
     }
     if (isneg){
-        return -nb-1;
+        return -nb;
     }else{
         return nb;
     }
 }
 
-function trunc(x){
-    // let nb =0;
-    // if (x> 0xffffffff){
-    //     x -= 0xffffffff;
-    //     nb += 0xffffffff;
-    // }
-    // let isneg = false;
-    // if (x<0){
-    //     isneg = true;
-    //     x = -x;
-    // }
-    // while (!(x < 1 && x > -1)){
-    //     x--;
-    //     nb++;
-    // }
-    // if (isneg){
-    //     return -nb;
-    // }else{
-    //     return nb;
-    // }
-}
-
 console.log(floor(3.0000001));
 
 console.log(ceil(1));
+
+console.log(round(1.5));
+
+
+console.log(trunc(0));
