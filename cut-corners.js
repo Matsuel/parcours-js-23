76ac53d491a435420af8250f33f6c04d847a71c0
Mathéo Lang <matheo.lang@ynov.com>
2023-03-26 19:26:25 +0200

    cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -1,63 +1,83 @@
 function round(x){
-    let rep=0;
+    let isneg = false;
     if (x<0){
-        while (x<-1){
-            x+=1;
-            rep+=1;
-        }
+        isneg = true;
+        x = -x;
     }else{
-        while (x>1){
+        let nb =0;
+        while (x>=1 && x <= -1){
             x-=1;
-            rep-=1;
+            nb+=1;
+        }
+        if (x>=0.5){
+            if (isneg){
+                return -nb;
+            }else{
+                return nb;
+            }
+        } else {
+            if (isneg){
+                return -nb-1;
+            }else{
+                return nb-1;
+            }
         }
     }
-    return rep;
 }
 
 function ceil(x){
-    let rep=0;
+    let isneg = false;
     if (x<0){
-        while (x<0){
-            x+=1;
-            rep+=1;
-        }
+        isneg = true;
+        x = -x;
     }else{
-        while (x>1){
+        let nb =0;
+        while (x>=1 && x >=0){
             x-=1;
-            rep+=1;
+            nb+=1;
+        }
+        if (isneg){
+            return -nb;
+        }else{
+            return nb+1;
         }
     }
-    return rep;
 }
 
 function floor(x){
-    let rep=0;
+    let isneg = false;
     if (x<0){
-        while (x<=-1){
-            x+=1;
-            rep+=1;
-        }
+        isneg = true;
+        x = -x;
     }else{
-        while (x>=0){
+        let nb =0;
+        while (x>=1 && x >=0){
             x-=1;
-            rep+=1;
+            nb+=1;
+        }
+        if (isneg){
+            return -nb-1;
+        }else{
+            return nb;
         }
     }
-    return rep;
 }
 
 function trunc(x){
-    let rep=0;
+    let isneg = false;
     if (x<0){
-        while (x<0){
-            x+=1;
-            rep+=1;
-        }
+        isneg = true;
+        x = -x;
     }else{
-        while (x>0){
+        let nb =0;
+        while (x>=1 && x >=0){
             x-=1;
-            rep+=1;
+            nb+=1;
+        }
+        if (isneg){
+            return -nb;
+        }else{
+            return nb;
         }
     }
-    return rep;
 }
