f90fcb3c2735133559a0fe1e81880bdbc168782a
Mathéo Lang <matheo.lang@ynov.com>
2023-03-27 10:58:20 +0200

    chunky

--- chunky.js
+++ chunky.js
@@ -1,9 +1,7 @@
 function chunk(arr, size){
     let rep = [];
-    for (let i=0; i<arr.length; i++){
-        for (let j = 0; j < size; j++){
-            rep.push(arr.slice(i, i+size));
-        }
+    for (let i=0; i<arr.length; i+=size){
+        rep.push(arr.slice(i, i+size));
     }
     return rep;
 }
