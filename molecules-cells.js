2b89c1d9ab4c6b57d4aaa5543652b59f15055e22
Mathéo Lang <matheo.lang@ynov.com>
2023-03-27 11:18:22 +0200

    molecules

--- molecules-cells.js
+++ molecules-cells.js
@@ -1,7 +1,24 @@
+const DNAtoRNA = {
+    G: "C",
+    C: "G",
+    T: "A",
+    A: "U",
+};
+const RNAtoDNA = {
+    C: "G",
+    G: "C",
+    A: "T",
+    U: "A",
+};
+
 function RNA(str){
     if (str===""){
         return "";
     }
+    return str
+        .split("")
+        .map((x)=> DNAtoRNA[x])
+        .join("");
 
 }
 
@@ -9,6 +26,12 @@ function DNA(str){
     if (str===""){
         return "";
     }
-
+    return str
+        .split("")
+        .map((x)=> RNAtoDNA[x])
+        .join("");
 
 }
+
+console.log(RNA('TAGC'))
+
