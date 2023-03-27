5396e55ed818b5fae1e16eb9780a9803e907fcc0
Mathéo Lang <matheo.lang@ynov.com>
2023-03-27 12:07:56 +0200

    flat

--- flat.js
+++ flat.js
@@ -1,8 +1,14 @@
-function flat(arr,int=1){
-    if (!Array.isArray(arr) || int===0){
+function flat(arr,int){
+    if (int===undefined) int=1;
+    if (!(Array.isArray(arr))){
+        return arr;
+    }
+    if (int ===0){
         return arr;
     }
     return arr.reduce((acc, curentarray)=>{
         return acc.concat(flat(curentarray,int-1))
-    })
+    },[])
 }   
+
+console.log(flat([1]))
