44f0262d03a847e355d979334bb61587a116f27c
Mathéo Lang <matheo.lang@ynov.com>
2023-03-27 16:10:09 +0200

    greedy-url

--- greedy-url.js
+++ greedy-url.js
@@ -1,5 +1,13 @@
 function getURL(str){
-    return str;
+    let rep=[];
+    let arr = str.split(" ");
+    var linkRegex = new RegExp(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/);
+    for (const url of arr){
+        if (url.match(linkRegex)){
+            rep.push(url)
+        }
+    }
+    return rep;
 }
 
 function greedyQuery(str){
