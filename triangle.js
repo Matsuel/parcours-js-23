bf27501e5e99ba5d3e8454775ef6bf9f4bef8812
Mathéo Lang <matheo.lang@ynov.com>
2023-03-27 11:33:26 +0200

    triangle

--- triangle.js
+++ triangle.js
@@ -1,9 +1,12 @@
 function triangle(str, int){
     let rep="";
-    for (let i=1; i<=int; i++){
-        rep+= rep + str.repeat(i)+"\n";
+    for (let i=0; i<=int; i++){
+        for (j=1;j<=i;j++){
+            rep+=str;
+        }
+        rep+="\n";
     }
-    return rep.slice(0,-1);
+    return rep.substring(1,rep.length-1);
 }
 
 console.log(triangle("*",5));
