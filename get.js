3de5938fe757307f5da4216bd772808cb55d76dd
Mathéo Lang <matheo.lang@ynov.com>
2023-03-27 11:23:15 +0200

    get

---
+++ get.js
@@ -0,0 +1,10 @@
+function get(src, path){
+    return path.split('.').reduce( function (obj, key){
+        if (!obj) return undefined;
+        return obj[key];
+    }, src);
+}
+
+const src = { nested: { key: 'peekaboo' } }
+const path = 'nested.key'
+console.log(get(src, path))
