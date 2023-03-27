b20a08a05cbb428d8320100a62a188cc0a6fd82b
Mathéo Lang <matheo.lang@ynov.com>
2023-03-27 15:10:56 +0200

    its-a-match

--- its-a-match.js
+++ its-a-match.js
@@ -1,7 +1,26 @@
-let normal= new RegExp('/hi/');
+let normal= new RegExp(/hi/);
 
-let begin = new RegExp('/^hi/');
+let begin = new RegExp(/^hi/);
 
-let end = new RegExp('/hi$/');
+let end = new RegExp(/hi$/);
 
-let beginEnd = new RegExp('/^hi$/^');
+let beginEnd = new RegExp(/^hi$/);
+
+
+console.log(normal.test('hi'));
+
+console.log(normal.test('voiture'));
+
+console.log(begin.test('hi w'));
+
+console.log(begin.test('w hi'));
+
+console.log(end.test('ersd hi'));
+
+console.log(end.test('hi w'));
+
+console.log(beginEnd.test('hi w'));
+
+console.log(beginEnd.test('fcgv hi'));
+
+console.log(beginEnd.test('vggvg hi sffe'));
