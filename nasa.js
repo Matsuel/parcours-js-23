4e84a30c32b37e6c58b9d6a8d728d925bac372c0
Mathéo Lang <matheo.lang@ynov.com>
2023-03-27 11:07:05 +0200

    nasa

--- nasa.js
+++ nasa.js
@@ -2,13 +2,16 @@ function nasa(int){
     let rep ="";
     for (let i=1; i<=int;i++){
         if (i%3===0 && i%5===0){
-            rep += "NASA ";
+            rep += "NASA";
         }else if (i%3===0){
-            rep += "NA ";
+            rep += "NA";
         }else if (i%5===0){
-            rep += "SA ";
+            rep += "SA";
         }else{
-            rep += i.toString() + " ";
+            rep += i.toString();
+        }
+        if (i<int){
+            rep += " ";
         }
     }
     return rep;
