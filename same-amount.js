ac33cda1d8d6f8481584400cfe1b17e622aec0cd
Mathéo Lang <matheo.lang@ynov.com>
2023-03-27 15:31:49 +0200

    same-amount

--- same-amount.js
+++ same-amount.js
@@ -1,3 +1,10 @@
 function sameAmount(str, regex1, regex2){
-    return true;
+    let rep= Boolean;
+    for (const letter of str){
+        if (letter.match(regex1) && letter.match(regex2)){
+            rep= false;
+        }
+        return false;
+    }
+    return rep;
 }
