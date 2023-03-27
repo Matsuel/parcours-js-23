05db2f53586b5608a3f58fbe35050ba58c3b7649
Mathéo Lang <matheo.lang@ynov.com>
2023-03-27 16:13:27 +0200

    greedy-url

--- greedy-url.js
+++ greedy-url.js
@@ -1,13 +1,6 @@
 function getURL(str){
-    let rep=[];
-    let arr = str.split(" ");
     var linkRegex = new RegExp(/https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}(\.[a-zA-Z0-9()]{1,6})?(?:[-a-zA-Z0-9()\[\],@:%_\+.~#?&\/=]*)/g);
-    for (const url of arr){
-        if (url.match(linkRegex)){
-            rep.push(url)
-        }
-    }
-    return rep;
+    return str.match(linkRegex);
 }
 
 function greedyQuery(str){
