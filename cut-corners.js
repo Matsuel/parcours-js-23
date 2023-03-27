db00696ce61c12246334c290e0923c05c4430d13
Mathéo Lang <matheo.lang@ynov.com>
2023-03-27 09:57:51 +0200

    cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -70,10 +70,8 @@ function trunc(x){
     if (Number.isNaN(x)|| (typeof(x)!== typeof(4))) return NaN;
     if (x>= 3000 || x<= -3000) return x;
     let nb =0;
-    if (x > 0xfffffffff) {
-        x -= 0xfffffffff;
-        nb += 0xfffffffff;
-    }
+    if (x >= 0xfffffffff) return 0xfffffffff;
+    if (x <= -0xfffffffff) return -0xfffffffff;
     let isneg = false;
     if (x<0){
         isneg = true;
