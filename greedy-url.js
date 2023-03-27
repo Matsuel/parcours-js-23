796c6cbfde3ba64be8ef6bc4f7f5d9532a739a46
Mathéo Lang <matheo.lang@ynov.com>
2023-03-27 16:22:38 +0200

    greedy-url

--- greedy-url.js
+++ greedy-url.js
@@ -1,10 +1,11 @@
 function getURL(str){
-    var linkRegex = new RegExp(/https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}(\.[a-zA-Z0-9()]{1,6})?(?:[-a-zA-Z0-9()\[\],@:%_\+.~#?&\/=]*)/g);
+    const linkRegex = new RegExp(/https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}(\.[a-zA-Z0-9()]{1,6})?(?:[-a-zA-Z0-9()\[\],@:%_\+.~#?&\/=]*)/g);
     return str.match(linkRegex);
 }
 
 function greedyQuery(str){
-    return str;
+    const linkRegexWith3Query= new RegExp(/\?([-a-zA-Z0-9\[\],()@]*=[-a-zA-Z0-9\[\],()@]*&){2,255}([-a-zA-Z0-9\[\],()@]*=[-a-zA-Z0-9\[\],()@]*)/g);
+    return str.match(linkRegexWith3Query);
 }
 
 function notSoGreedy(str){
