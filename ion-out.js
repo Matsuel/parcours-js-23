d27e0d9cfe3bf7d19060c2f4cfd25e0ed85b39b6
Mathéo Lang <matheo.lang@ynov.com>
2023-03-27 15:58:30 +0200

    ion-out

--- ion-out.js
+++ ion-out.js
@@ -2,7 +2,7 @@ function ionOut(str){
     let rep= [];
     let arr= str.split(" ");
     let regexiont = new RegExp(/tion/g);
-    let regexion= new RegExp(/ion/g);
+    let regexion= new RegExp(/[.,?]ion/g);
     for (const word of arr){
         if (word.match(regexiont)){
             rep.push(word.replace(regexion, ""))
