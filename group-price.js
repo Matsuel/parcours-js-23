45f0fd509bc5b49a0d1efaed3944bf1a7f83042d
Mathéo Lang <matheo.lang@ynov.com>
2023-03-27 16:52:19 +0200

    group-price

--- group-price.js
+++ group-price.js
@@ -1,19 +1,24 @@
 function groupPrice(str){
     let rep=[];
+    let pricearr=[];
     let regexprice= new RegExp(/(([A-Z]{3})|\$)([0-9]+\.[0-9]+)/,"g");
     let prices= str.match(regexprice);
     if (prices===null) return rep;
     else{
         for(const price of prices){
-            rep.push(price);
-            rep.push(price.match(/[0-9]+/g)[0]);
-            rep.push(price.match(/[0-9]+/g)[1]);
+            pricearr=[];
+            pricearr.push(price);
+            pricearr.push(price.match(/[0-9]+/g)[0]);
+            pricearr.push(price.match(/[0-9]+/g)[1]);
+            rep.push(pricearr)
         }
     }
 
-    return [rep];
+    return rep;
 }
 
 console.log(groupPrice("USD12.31"))
 
 console.log(groupPrice('The price of the cereals is $4.00.'))
+
+console.log(groupPrice('product one its $9.98 and the second one its $10.20'))
