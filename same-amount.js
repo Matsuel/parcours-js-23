31b4816b322369236f6497d52e98987305727dc3
Mathéo Lang <matheo.lang@ynov.com>
2023-03-27 15:37:18 +0200

    same-amount

--- same-amount.js
+++ same-amount.js
@@ -1,3 +1,5 @@
 function sameAmount(str, regex1, regex2){
-    return str.match(regex1) && str.match(regex2);
+    return str.match(regex1)!==null && str.match(regex2)!=null;
 }
+
+console.log(sameAmount("data", /q /, /qqqqqqq/))
