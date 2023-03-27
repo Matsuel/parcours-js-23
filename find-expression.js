37b816d0e94c874ae84e86e6baad3e92f775f718
Mathéo Lang <matheo.lang@ynov.com>
2023-03-27 14:05:27 +0200

    findexpression

--- find-expression.js
+++ find-expression.js
@@ -1,10 +1,19 @@
+const add4= '+4'
+const mul2= '*2'
+function findExpression(number){
+    if (number===undefined) return undefined;
+    let nb=1;
+    let rep="1";
 
-function findExpression(number,rep, nb){
-    // console.log(number)
-    if (!(number%2===0) && rep===undefined) return undefined;
-    if (rep===undefined && nb===undefined) rep='1'+" "+mul2, nb=1;
-    if (number/2<= number-4) return findExpression(number/2, rep+" "+mul2, nb*2)
-    if (number===0) return rep;
-    else return findExpression(number-4, rep+" "+add4, nb+4)
+    while (nb<=number){
+        if (nb === number) return rep;
+        if (nb+4 === number){
+            rep+= " "+ add4;
+            nb+=4;
+        }
+        else rep+= " "+mul2, nb*=2;
+    }
+    return rep;
 }
 
+console.log(findExpression(8))
