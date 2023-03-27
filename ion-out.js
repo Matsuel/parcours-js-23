112d7df43e09b2aa5de7c86ffc2de0d1a80ef6dc
Mathéo Lang <matheo.lang@ynov.com>
2023-03-27 16:03:59 +0200

    ion-out

--- ion-out.js
+++ ion-out.js
@@ -5,8 +5,16 @@ function ionOut(str){
     let regexion= new RegExp(/ion/g);
     for (const word of arr){
         if (word.match(regexiont)){
-            rep.push(word.replace(regexion, ""))
+            console.log(word.includes(","))
+            if (word.includes(".")|| word.includes(",")|| word.includes("?")){
+                let newword= word.replace(regexion,"")
+                rep.push(newword.substring(0,newword.length-1))
+            }else{
+                rep.push(word.replace(regexion, ""))
+            }
         }
     }
     return rep;
 }
+
+console.log(ionOut('attention, direction'))
