660383d4e3b4a08b77ad669364b6d8170160e381
Mathéo Lang <matheo.lang@ynov.com>
2023-03-30 14:31:40 +0200

    count-leap-years

---
+++ count-leap-years.js
@@ -0,0 +1,11 @@
+function countLeapYears (date){
+    let years= date.getFullYear();
+    let count=0;
+
+    for (let i=0; i<date;i++){
+        if (i%4===0){
+            count++;
+        }
+    }
+    return count;
+}
