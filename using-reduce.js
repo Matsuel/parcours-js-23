1029073800257b45f7117c0b56097a7266602b6a
Mathéo Lang <matheo.lang@ynov.com>
2023-03-30 16:14:57 +0200

    using-reduce

--- using-reduce.js
+++ using-reduce.js
@@ -1,7 +1,4 @@
-function adder(arr,n=0){
-    if (arr.length===0){
-        return n;
-    }
-    let rep= arr.reduce((acc, obj) => acc + obj);
-    return rep+n;
+function adder(arr,n){
+    return arr.reduce((acc, obj) => acc + obj) + n;
+
 }
