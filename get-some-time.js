3235f6ef9eb95b2bdb56f6ab48967985e53f14b8
Mathéo Lang <matheo.lang@ynov.com>
2023-03-30 11:57:35 +0200

    get-some-times

--- get-some-time.js
+++ get-some-time.js
@@ -1,66 +1,52 @@
-const months={
-    0:31,
-    1:28,
-    2:31,
-    3:30,
-    4:31,
-    5:30,
-    6:31,
-    7:31,
-    8:30,
-    9:31,
-    10:30,
-    11:31
+function firstDayWeek(nbWeek, year) {
+    if (nbWeek===1) return "01-01-"+year;
+    let rep ="";
+    let date = getDateFromWeek(nbWeek, year);
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let yearDate = date.getFullYear();
+    if (day < 10) {
+        rep += "0" + day;
+    }else {
+        rep += day-2;
+    }
+    if (month < 10) {
+        rep += "-0" + month;
+    }else {
+        rep += "-" + month;
+    }
+    rep += "-" + yearDate;
+    let d = new Date(yearDate, month, day);
+    console.log(d.getDay())
+    return rep;
 }
 
-function firstDayWeek(number, string){
-    if (number===1) return "0"+String(number)+"-01-"+string;
-    let daysofyear= DayofYear();
-    console.log(daysofyear);
-    let day=1;
-    let month=0;
-    number*=7;
-    while(number-7>=0){
-        if (day+7> months[month]){
-            month+=1;
-            day+=7;
-            day-=months[month];
-            number-=7;
-        }else{
-            number-=7;
-            day+=7;
+function getDaysInYear(year) {
+    let days = 0;
+    if (parseInt(year) % 4 === 0) {
+        for (let i = 1; i <= 12; i++) {
+            days += monthsDaysYearBisex[i];
         }
-        console.log(day+"-"+month+"-"+string)
-    }
-    month+=1;
-    if ((day*month)%7!==0){
-        day-=7;
-    }
-    let d= new Date(day, month, parseInt(string));
-    day-= d.getDay();
-    return day+"-"+month+"-"+string;
-
-}
-
-function DayofYear(){
-    let days=0;
-    for(let i=0; i<12;i++){
-        days+= months[i];
+    }else {
+        for (let i = 1; i <= 12; i++) {
+            days += monthsDays[i];
+        }
     }
     return days;
 }
 
+function getDateFromWeek(week, year) {
+    let date = new Date(year, 0, 1 + (week-1) * 7);
+    return date;
+}
+
 let one = firstDayWeek(1, '1000')
 let two = firstDayWeek(52, '1000')
+let three= firstDayWeek(2, '2022')
 
 console.log("=============== RESULT 1 =====================")
 console.log(one)
 console.log("=============== RESULT 2 =====================")
-console.log(two)
+console.log(two)  
 
-
-// 364 mod 7= 0
-
-// donc -7 pour lundi
-
-// 364-7=357
+console.log(three)
