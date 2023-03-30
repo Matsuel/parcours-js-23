1cecca3024bd8e2b178f55438c84bad4b4092517
Mathéo Lang <matheo.lang@ynov.com>
2023-03-30 16:20:03 +0200

    using-reduce

--- using-reduce.js
+++ using-reduce.js
@@ -4,3 +4,10 @@ function adder(arr,n){
     );
 
 }
+
+function sumOrMul (arr,n){
+    return arr.reduce((acc, obj) => 
+        obj % 2 === 0 ? acc * obj : acc + obj,
+        n===undefined? 0 : n
+    );
+}
