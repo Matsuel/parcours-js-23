cfd394b7a266a4e0febc9945ceb58cc519669935
Mathéo Lang <matheo.lang@ynov.com>
2023-03-30 14:08:31 +0200

    rebecca-black

---
+++ rebecca-black.js
@@ -0,0 +1,15 @@
+function isFriday(date){
+    return date.getDay() === 5;
+}
+
+function isWeekend(date){
+    return date.getDay() === 0 || date.getDay() === 6;
+}
+
+function isLeapYear(date){
+    return date.getFullYear() % 4 === 0;
+}
+
+function isLastDayOfMonth(date){
+    return date.getDate() === new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+}
