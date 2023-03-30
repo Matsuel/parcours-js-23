bb49cef144fe166f075d0868e8df59012b2ebb4f
Mathéo Lang <matheo.lang@ynov.com>
2023-03-30 14:18:21 +0200

    match-cron

--- match-cron.js
+++ match-cron.js
@@ -2,10 +2,8 @@ function matchCron(cron, date){
     let crons = cron.split(" ");
     let day = date.getDate();
     let month = date.getMonth() + 1;
-    let year = date.getFullYear();
     let hour = date.getHours();
     let minute = date.getMinutes();
-    let second = date.getSeconds();
     let dayOfWeek = date.getDay();
-    return crons[0] === second.toString() && crons[1] === minute.toString() && crons[2] === hour.toString() && crons[3] === day.toString() && crons[4] === month.toString() && crons[5] === dayOfWeek.toString();
+    return crons[0] === minute && crons[1]=== hour && crons[2]=== day && crons[3] === month && crons[4] === dayOfWeek;
 }
