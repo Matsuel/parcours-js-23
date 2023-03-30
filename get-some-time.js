9b979255ddf2bb55bff4eadda4c120db4398602a
Mathéo Lang <matheo.lang@ynov.com>
2023-03-30 09:43:48 +0200

    get-some-times

--- get-some-time.js
+++ get-some-time.js
@@ -1,4 +1,14 @@
 function firstDayWeek(number, string){
-    let rep="";
-    
+    let rep="01-";
+    if (number<10){
+        rep+="0"+String(number)+"-";
+    }else{
+        rep+=String(number)+"-";
+    }
+
+    rep+=string;
+    return rep;
+
 }
+
+console.log(firstDayWeek(1, '1000'))
