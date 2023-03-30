92df4f6f51863a341c81efd8add8407746d3ef71
Mathéo Lang <matheo.lang@ynov.com>
2023-03-30 14:51:09 +0200

    day-of-the-year

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -4,13 +4,10 @@ function dayOfTheYear(date){
     
     let difference_time= date.getTime()- dayOneOfYear.getTime();
 
-    let difference_day= difference_time/ (1000*3600*24);
+    let difference_day= difference_time/ (86400000);
 
     return Math.round(difference_day);
 }
-
-// console.log(dayOfTheYear(Date.now()))
-
 
 let d = new Date("2022-08-03");
 
