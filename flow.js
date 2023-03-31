b98cefbe32f518b1c41189459265391998ceaf71
Mathéo Lang <matheo.lang@ynov.com>
2023-03-31 11:24:01 +0200

    _.flow([funcs])

--- flow.js
+++ flow.js
@@ -1,5 +1,8 @@
 function flow(el) {
     return function(...args){
+        if (args.length >1 ){
+            args= [arr[0](...args)];
+        }
         return args.reduce((acc, fn) => fn(acc), el[0])
     }
 }
