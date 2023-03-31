894d37bcbeced0d7e2e678b8824fdbc80c30aaf0
Mathéo Lang <matheo.lang@ynov.com>
2023-03-31 12:20:31 +0200

    get-them-all

--- get-them-all.js
+++ get-them-all.js
@@ -1,30 +1,29 @@
 function getArchitects() {
     return [
-        document.querySelectorAll('body a'),
-        document.querySelectorAll('body span'),
-
-    ]
+        document.querySelectorAll("body a"),
+        document.querySelectorAll("body span"),
+    ];
 }
 
 function getClassical()  {
     return [
-        document.getElementsByClassName('a.classical'),
-        document.getElementsByClassName('a:not(.classical)'),
-    ]
+        document.getElementsByClassName("a.classical"),
+        document.getElementsByClassName("a:not(.classical)"),
+    ];
 }
 
 function getActive ()  {
     return [
-        document.querySelectorAll('a.classical.active'),
-        document.querySelectorAll('a.classical:not(.active)'),
-    ]
+        document.querySelectorAll("a.classical.active"),
+        document.querySelectorAll("a.classical:not(.active)"),
+    ];
 }
 
 function getBonannoPisano()  {
     return [
-        document.getElementById('bonanno-pisano'),
-        document.querySelectorAll('a.classical.active'),
-    ]
+        document.getElementById("bonanno-pisano"),
+        document.querySelectorAll("a.classical.active"),
+    ];
 }
 
 export { getArchitects, getClassical, getActive, getBonannoPisano };
