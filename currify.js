a07fbf0eb274388cb2864660c5fd1c4cff8f01c2
Mathéo Lang <matheo.lang@ynov.com>
2023-03-31 11:30:45 +0200

    currify

--- currify.js
+++ currify.js
@@ -1,6 +1,6 @@
 function currify(el){
     return function currified(...args){
-        return args.length >= el.length
+        return args.length >= fn.length
         ? fn(...args)
         : currified.bind(null, ...args);
     };
