f5c86dda2df046cd63a5132232b48f8da45e178b
Mathéo Lang <matheo.lang@ynov.com>
2023-04-01 12:58:58 +0200

    get-them-all

--- get-them-all.js
+++ get-them-all.js
@@ -1,6 +1,6 @@
 export const  getArchitects= () => {
-    let a = document.getElementsByTagName('a');
-    let other = document.getElementsByTagName('span');
+    let a = document.getElementsByTagName('a')
+    let other = document.getElementsByTagName('span')
     return [
         a,
         other,
