ebf8ebeab46d375532aea9b21fac3cbbe6a12701
Mathéo Lang <matheo.lang@ynov.com>
2023-04-03 09:46:11 +0200

    build-bricks

---
+++ build-brick-and-break.js
@@ -0,0 +1,31 @@
+export function build(numberOfBricks){
+    let i=1;
+    for (i=1; i<=numberOfBricks; i++){
+        setInterval(function(){
+            let block=document.createElement("div");
+            block.id="brick-"+String(i);
+            document.body.append(block);
+            block.innerHTML=String(i);
+            if ((i+1)%3===0) block.dataset.foundation= "true";
+            i++;
+        }, 100);
+    }
+    
+
+}
+
+export function repair(number){
+    for (let i=1; i<=number; i++){
+        let block= document.getElementById("brick-"+String(i));
+        if ((i+1)%3===0){
+            block.dataset.repaired= "in progress";
+        }else{
+            block.dataset.repaired= "true";
+        }
+    }
+}
+
+export function destroy(){
+    let bricks= document.querySelectorAll("div[id^='brick-']");
+    bricks[bricks.length-1].remove();
+}
