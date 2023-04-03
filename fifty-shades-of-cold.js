f7e8777f2b706a8870208442a923e72425814339
Mathéo Lang <matheo.lang@ynov.com>
2023-04-03 14:38:21 +0200

    fifty

---
+++ fifty-shades-of-cold.js
@@ -0,0 +1,24 @@
+import {Colors } from './fifty-shades-of-cold.data.js';
+
+export function generateClasses(){
+    const style= document.createElement('style');
+    Colors.forEach(color => {
+        style.innerHTML += `.${color} {\n background: ${color};\n}\n\n`;
+    });
+    document.head.appendChild(style);
+}  
+
+export function generateColdShades(){
+    Colors.forEach(color => {
+        const div = document.createElement('div');
+        div.classList.add(color);
+        div.innerHTML = color;
+        document.body.appendChild(div);
+    });
+}
+
+export function choseShade(shade){
+    document.querySelectorAll('div').forEach(div => {
+        div.className = shade;
+    });
+}
