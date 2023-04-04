5d6465013c30b37fd6e5e907302cb559198286c6
Mathéo Lang <matheo.lang@ynov.com>
2023-04-04 11:23:38 +0200

    mouse-trap

--- mouse-trap.js
+++ mouse-trap.js
@@ -1,111 +1,71 @@
-let circles = [];
-let box;
-
-class Circle{
-    constructor(x, y){
-        this.x = x;
-        this.y = y;
-        this.HTML= null;
-        this.isTrapped = false;
-        this.size= 50;
-        this.draw();
-        circles.push(this);
-    }
-    draw(){
-        this.HTML = document.createElement('div');
-        this.HTML.classList.add('circle');
-        this.HTML.style.position = 'absolute';
-        this.HTML.style.left = this.x + 'px';
-        this.HTML.style.top = this.y + 'px';
-        this.HTML.style.background = 'white';
-        this.trap();
-        document.body.appendChild(this.HTML);
-    }
-
-    trap(){
-        if (this.x > box.x && this.y> box.y && this.x+ this.size< box.x + box.width && this.y+ this.size< box.height + box.y){
-            this.isTrapped = true;
-            this.HTML.style.background = "var(--purple)";
-        }else{
-            this.isTrapped = false;
-            this.HTML.style.background = "white";
-        }
-    }
-
-    move(x, y) {
-        this.trap();
-        if (!this.isTrapped) {
-            this.x = x;
-            this.y = y;
-            this.HTML.style.top = this.y + "px";
-            this.HTML.style.left = this.x + "px";
+let box
+let flag = true
+let x
+let y
+let circle
+export function createCircle() {
+    addEventListener("click", function () {
+        circle = document.createElement("div")
+        circle.className = "circle"
+        if (flag) {
+            circle.style.background = "white"
+            circle.style.left = x
+            circle.style.top = y
         } else {
-            if (this.inReactangle(x, y)) {
-                this.x = x;
-                this.y = y;
-                this.HTML.style.top = this.y + "px";
-                this.HTML.style.left = this.x + "px";
-            } else {
-                if (this.inReactangle(x, this.y)) {
-                    this.x = x;
-                    this.HTML.style.left = this.x + "px";
-                } else if (this.inReactangle(this.x, y)) {
-                    this.y = y;
-                    this.HTML.style.top = this.y + "px";
-                }
+            circle.style.background = 'var(--purple)'
+            circle.style.left = x
+            circle.style.top = y
+        }
+        document.body.appendChild(circle)
+        flag = true
+    })
+}
+export function moveCircle() {
+    addEventListener("mousemove", e => {
+        document.querySelectorAll(".circleRem").forEach((elem) => {
+            elem.remove()
+        })
+        x = e.clientX - 25 + "px"
+        y = e.clientY - 25 + "px"
+        let circle = document.createElement("div")
+        circle.className = "circle"
+        circle.classList.add("circleRem")
+        if (flag) {
+            circle.style.background = "white"
+        } else {
+            circle.style.background = 'var(--purple)'
+        }
+        circle.style.left = e.clientX - 25 + "px"
+        circle.style.top = e.clientY - 25 + "px"
+
+        document.body.appendChild(circle)
+        if ((e.clientX >= box.getBoundingClientRect().left + 25 && e.clientX <= box.getBoundingClientRect().right - 25) && (e.clientY >= box.getBoundingClientRect().top + 25 && e.clientY <= box.getBoundingClientRect().bottom - 25)) {
+            document.querySelector(".circle").style.background = 'var(--purple)'
+            flag = false
+        }
+        if (!flag) {
+            if (e.clientX - 25 < box.getBoundingClientRect().left) {
+                circle.style.left = box.getBoundingClientRect().left + "px"
+                document.querySelector(".circle").style.background = 'var(--purple)'
+            }
+            if (e.clientX + 25 > box.getBoundingClientRect().right) {
+                circle.style.left = box.getBoundingClientRect().right - 50 + "px"
+                document.querySelector(".circle").style.background = 'var(--purple)'
+            }
+            if (e.clientY - 25 < box.getBoundingClientRect().top) {
+                circle.style.top = box.getBoundingClientRect().top + "px"
+                document.querySelector(".circle").style.background = 'var(--purple)'
+            }
+            if (e.clientY + 25 > box.getBoundingClientRect().bottom) {
+                circle.style.top = box.getBoundingClientRect().bottom - 50 + "px"
+                document.querySelector(".circle").style.background = 'var(--purple)'
             }
         }
-    }
+    })
 }
-
-class Box {
-    constructor() {
-        this.HTML = document.createElement("div");
-        this.HTML.classList.add("box");
-        this.HTML.style.position = "absolute";
-        this.HTML.style.top = "50%";
-        this.HTML.style.left = "50%";
-        this.HTML.style.transform = "translate(-50%, -50%)";
-        document.body.appendChild(this.HTML);
-        this.x = this.HTML.offsetLeft - this.HTML.offsetWidth / 2 - 1; // -1 to account for the border
-        this.y = this.HTML.offsetTop - this.HTML.offsetHeight / 2 - 1;
-        this.width = this.HTML.offsetWidth + 1; // +1 to account for the border
-        this.height = this.HTML.offsetHeight + 1;
-    }
-
-    inReactangle(x, y) {
-        if (
-            x > box.x &&
-            x + this.size < box.x + box.width &&
-            y > box.y &&
-            y + this.size < box.y + box.height
-        ) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
-
-document.body.addEventListener('click', event =>{
-    createCircle(event)
-});
-
-document.body.addEventListener('mousemove', event =>{
-    moveCircle(event)
-});
-
-
-export function createCircle(event){
-    if (event === undefined) return;
-    new Circle(event.clientX-25, event.clientY-25);
-}
-
-export function moveCircle(event){
-    if (event === undefined || circles.length ===0) return;
-    circles[circles.length-1].move(event.clientX-25, event.clientY-25);
-}
-
-export function setBox(){
-    box = new Box();
+export function setBox() {
+    box = document.createElement("div")
+    box.className = "box"
+    document.body.appendChild(box)
+    console.log(box.getBoundingClientRect().bottom)
 }
