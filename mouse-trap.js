857cccbb1d3a717cccc605faa9870b8b2c1a89b4
Mathéo Lang <matheo.lang@ynov.com>
2023-04-04 10:10:55 +0200

    mouse-trap

---
+++ mouse-trap.js
@@ -0,0 +1,41 @@
+document.addEventListener('click', function(event){
+    createCircle(event)
+});
+
+let circles = [];
+let box;
+
+class Circle{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.HTML= null;
+        this.isTrapped = false;
+        this.size= 50;
+        this.draw();
+        circles.push(this);
+    }
+    draw(){
+        this.HTML = document.createElement('div');
+        this.HTML.classList.add('circle');
+        this.HTML.style.left = this.x + 'px';
+        this.HTML.style.top = this.y + 'px';
+        this.HTML.style.background = 'white';
+        this.trap();
+        document.body.appendChild(this.HTML);
+    }
+
+    trap(){
+        if (this.x > box.x && this.y> box.y && this.x+ this.size< box.size && this.y+ this.size< box.size){
+            this.isTrapped = true;
+            this.HTML.style.background = "var(--purple)";
+        }else{
+            this.isTrapped = false;
+            this.HTML.style.background = "white";
+        }
+    }
+}
+
+function createCircle(event){
+
+}
