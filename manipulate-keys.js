059ff4ba4f0bf220d156fac30fd8473ecd000c26
Mathéo Lang <matheo.lang@ynov.com>
2023-04-04 16:01:44 +0200

    manipulated-keys

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -23,7 +23,7 @@ function reduceKeys(db, reduce, acc= ""){
     for (let key in db) {
         acc = reduce(acc,key);
     }
-    return acc.substring(1, acc.length);
+    return acc.substring(2, acc.length);
 }
 
 const nutrients = { carbohydrates: 12, protein: 20, fat: 5 }
