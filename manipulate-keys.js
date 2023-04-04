6fe8b74871f4bb1034229d7d044e751fe9b0d3fe
Mathéo Lang <matheo.lang@ynov.com>
2023-04-04 16:01:19 +0200

    manipulated-keys

---
+++ manipulate-keys.js
@@ -0,0 +1,38 @@
+function filterKeys(db,filter){
+    let rep={};
+    for (let key in db) {
+        if (!db.hasOwnProperty(key))  continue;
+        if (filter(key)) {
+            rep[key] = db[key];
+        }
+    }
+    return rep;
+}
+
+function mapKeys(db,m){
+    let rep={};
+    for (let key in db) {
+        if (!db.hasOwnProperty(key))  continue;
+        rep[m(key)] = db[key];
+    }
+    return rep;
+}
+
+
+function reduceKeys(db, reduce, acc= ""){
+    for (let key in db) {
+        acc = reduce(acc,key);
+    }
+    return acc.substring(1, acc.length);
+}
+
+const nutrients = { carbohydrates: 12, protein: 20, fat: 5 }
+
+console.log(filterKeys(nutrients, (key) => /protein/.test(key)))
+// output: { protein: 20 }
+
+console.log(mapKeys(nutrients, (k) => `-${k}`))
+// output: { -carbohydrates: 12, -protein: 20, -fat: 5 }
+
+console.log(reduceKeys(nutrients, (acc, cr) =>acc.concat(', ', cr)))
+// output: carbohydrates, protein, fat
