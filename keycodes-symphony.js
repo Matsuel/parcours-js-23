78619f8d453b7de1f4e0e21fea300554e957bf29
Mathéo Lang <matheo.lang@ynov.com>
2023-04-04 09:37:59 +0200

    keycodes

--- keycodes-symphony.js
+++ keycodes-symphony.js
@@ -8,7 +8,7 @@ export function compose(event){
         let div = document.createElement('div');
         div.classList.add('note');
         div.innerHTML = event.key;
-        div.style.backgroundColor= `rgb( ${(255/26)* (event.key.charCodeAt(0))}, ${(255/26)* (event.key.charCodeAt(0))}, ${(255/26)* (event.key.charCodeAt(0))})`;
+        div.style.backgroundColor= `rgb( ${(255/26)* (event.keyCode)}, ${(255/26)* (event.keyCode)}, ${(255/26)* (event.keyCode)})`;
         document.body.appendChild(div);
     }else if (event.key === 'Backspace'){
         document.body.removeChild(document.body.lastElementChild);
