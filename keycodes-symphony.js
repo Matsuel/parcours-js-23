aa6e48c404ff979cc6e2b7efa0568c99b5fb1338
Mathéo Lang <matheo.lang@ynov.com>
2023-04-04 09:22:59 +0200

    keycodes

--- keycodes-symphony.js
+++ keycodes-symphony.js
@@ -3,6 +3,7 @@ document.addEventListener('keydown', function(e) {
 });
 
 export function compose(event){
+    if (event=== undefined) return;
     if (event.keyCode >= 65 && event.keyCode <= 90 || event.keyCode >= 97 && event.keyCode <= 122){
         let div = document.createElement('div');
         div.classList.add('note');
