0f029426d9172b20ff07b81b16e37f9670f87a7a
Mathéo Lang <matheo.lang@ynov.com>
2023-04-04 09:05:05 +0200

    fifty

--- fifty-shades-of-cold.js
+++ fifty-shades-of-cold.js
@@ -9,7 +9,7 @@ export function generateClasses(){
 }  
 
 export function generateColdShades(){
-    let cold = ['aliceblue','aqua','aquamarine', 'blue', 'turquoise', 'green','blueviolet', 'cyan', 'navy', 'purple']
+    let cold = ['aliceblue','aqua','aquamarine', 'blue', 'turquoise', 'green','blueviolet','cadetblue', 'cyan', 'navy', 'purple']
     for(let color of colors){
         if (cold.indexOf(color) > -1){
             let div = document.createElement('div');
