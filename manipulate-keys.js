995fcf78420c1758f1a5a7fc167bd57ad9bf872e
Mathéo Lang <matheo.lang@ynov.com>
2023-04-06 09:38:50 +0200

    manipulated

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -28,7 +28,7 @@ function reduceKeys(db, reduce, acc= ""){
     for (let key in db) {
         acc = reduce(acc,key);
     }
-    if (acc[0]=== ":" && !acc[acc.length-1]=== ":"){
+    if (acc[0]=== ":" && acc[acc.length-1]!== ":"){
         return acc.substring(1, acc.length);
     }
     if (acc.slice(0,2)===", "){
