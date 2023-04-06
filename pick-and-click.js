68649a9a5114fe4dbfa4bb925ff2a376ad879de2
Mathéo Lang <matheo.lang@ynov.com>
2023-04-06 11:10:50 +0200

    pick-and-click

--- pick-and-click.js
+++ pick-and-click.js
@@ -19,15 +19,44 @@ const luminosity = document.createElement('div');
 luminosity.classList.add('luminosity', 'text');
 document.body.appendChild(luminosity);
 
+const svg = document.createElement("svg")
+svg.id = "svg";
+svg.setAttribute("width", "100%");
+svg.setAttribute("height", "100%");
+svg.setAttribute("viewBox", "0 0 100 100");
+svg.setAttribute("preserveAspectRatio", "none");
+
+const axiX = document.createElementNS("http://www.w3.org/2000/svg", "line");
+axiX.id = "axisX";
+axiX.setAttribute("width", "100%");
+axiX.setAttribute("height", "100%");
+axiX.setAttribute("x1", "0");
+axiX.setAttribute("x2", "0");
+svg.appendChild(axiX);
+
+const axiY = document.createElementNS("http://www.w3.org/2000/svg", "lien");
+axiY.id = "axisY";
+axiY.setAttribute("width", "100%");
+axiY.setAttribute("height", "100%");
+axiY.setAttribute("y1", "0");
+axiY.setAttribute("y2", "0");
+svg.appendChild(axiY);
+
 export function pick(event){
     if (event=== undefined) return;
-    const hue = Math.round(event.clientX / window.innerWidth * 360);
-    const luminosity = Math.round(event.clientY / window.innerHeight * 100);
-    const color = `hsl(${hue}, 100%, ${luminosity}%)`;
+    const huevalue = Math.round(event.clientX / window.innerWidth * 360);
+    const lum = Math.round(event.clientY / window.innerHeight * 100);
+    const color = `hsl(${huevalue}, 100%, ${lum}%)`;
     document.body.style.background = color;
     hsl.innerHTML = color;
-    hue.innerHTML = `${String(hue)}`;
-    luminosity.innerHTML = `${String(luminosity)}`;
+    hue.innerHTML = `${String(huevalue)}`;
+    luminosity.innerHTML = `${String(lum)}`;
+    axiX.setAttribute("x1", event.clientX);
+    axiX.setAttribute("x2", `${event.clientX}`);
+    axiY.setAttribute("y1", `${event.clientY}`);
+    axiY.setAttribute("y2", `${event.clientY}`);
+    console.log(axiX.getAttribute("x1"));
+
 }
 
 async function copyToClipboard(){
