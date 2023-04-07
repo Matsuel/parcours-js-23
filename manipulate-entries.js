f00d560804c6d9dfa68f2714f355395662aa6f46
Mathéo Lang <matheo.lang@ynov.com>
2023-04-07 15:14:50 +0200

    entries

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -1,1 +1,14 @@
-function filterEntries()
+function filterEntries(object,filter){
+    let rep={};
+    for (let key in object){
+        if (filter([key,object[key]])){
+            rep[key]=object[key];
+        }
+    }
+    return rep;
+}
+
+function mapEntries(object,map){
+    let rep ={};
+    
+}
